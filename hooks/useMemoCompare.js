a418d18bef5fbc379b38f5c25530acf6cbd78569
Supphachok Phetkaew <supphachok.pk@gmail.com>
2022-02-10 13:19:20 +0700

    create useMemoCompare hook

---
+++ hooks/useMemoCompare.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react"
+
+const useMemoCompare = (next, compare) => {
+  const previousRef = useRef()
+  const previous = previousRef.current
+
+  const isEqual = compare(previous, next)
+
+  useEffect(() => {
+    if (!isEqual) {
+      previousRef.current = next
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return isEqual ? previous : next
+}
+
+export default useMemoCompare
